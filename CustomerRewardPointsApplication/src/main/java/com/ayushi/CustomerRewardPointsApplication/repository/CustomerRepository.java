4c1fa3356c134d99dc4f75d17b213330c2112891
ayushi-garg-27 <87812785+ayushi-garg-27@users.noreply.github.com>
2025-01-15 00:29:02 +0530

    Delete CustomerRewardPointsApplication directory

--- CustomerRewardPointsApplication/src/main/java/com/ayushi/CustomerRewardPointsApplication/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ayushi.CustomerRewardPointsApplication.repository;
-import com.ayushi.CustomerRewardPointsApplication.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByCustomerId(String customerId);
-}
-
